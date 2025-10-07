0cb1217bd3964926bf1158fa10e10487e447992b
Heidi <heidimanobanda@gmail.com>
2025-10-07 10:26:32 -0500

    Agregar archivos de proyecto.

---
+++ Patrones1P/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Patrones1P
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
